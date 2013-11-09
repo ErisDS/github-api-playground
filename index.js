d59253ea669b679a00948c5117deeeac745cf3f8
Hannah Wolfe <erisds@gmail.com>
2013-11-09 15:05:23 +0000

    First stab at a roadmap listing tool

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var GitHubAPI   = require('github'),
     _           = require('underscore'),
     nodefn      = require('when/node/function'),
-    GitHub,
+
+    /// vars
+    GitHub      = new GitHubAPI({
+        version: '3.0.0'
+    }),
     repo,
     milestone;
 
-// This should be config
+
+// ## Configuration - should be abstracted
+
 repo = {
     user: 'TryGhost',
     repo: 'Ghost'
@@ -14,28 +20,69 @@ repo = {
 milestone = 3;
 
 
-GitHub = new GitHubAPI({
-    version: '3.0.0'
-});
+// ## Formatting
 
 function isBug(issue) {
     if (issue.labels) {
-        console.log(_.pluck(issue.labels, 'name'));
+        return _.contains(_.pluck(issue.labels, 'name'), 'bug');
     }
+}
+
+function process(items) {
+    var issues = [],
+        bugs = [];
+
+    _.each(items, function (item) {
+        if (isBug(item)) {
+            bugs.push(item);
+        } else {
+            issues.push(item);
+        }
+    });
+
+    return {issues: issues, bugs: bugs};
+}
+
+function labelList(issue) {
+
+    return _.reduce(issue.labels, function (memo, label) {
+        var out = "";
+
+        if (label.name !== "bug") {
+            if (memo !== "") {
+                out += ", ";
+            }
+
+            out += label.name;
+        }
+
+        return memo + out;
+    }, "");
 }
 
 function issueToListItem(issue) {
     var status = issue.state === 'open' ? '' : '~~',
-        link =  issue.title + "[" + status + issue.number + status + "](" + issue.html_url + ")"
+        labels = labelList(issue),
+        link =   "[" + status + '#' + issue.number + status + "](" + issue.html_url + ")";
 
-    //console.log(issue.pull_request);
-    return issue.title + "[" + status + issue.number + status + "](" + issue.html_url + ")";
+    return issue.title + (labels !== "" ? " (" + labels + ") " : " ") + link;
+}
+
+
+// ## Fetch Data
+
+function getMilestone() {
+
+}
+
+function getPullRequests() {
+
 }
 
 function getIssuePage(page) {
     var options = _.extend(repo, {
         milestone: milestone,
-        sort: 'updated',
+        sort: 'created',
         per_page: 100,
         page: page
     });
@@ -47,10 +94,20 @@ function loadIssues() {
     var page = 1;
 
     // Todo: recursive pagination
-    getIssuePage(page).then(function (issues) {
-        console.log(issues.meta);
-        _.each(issues, function (issue) {
+    getIssuePage(page).then(function (items) {
+        items = process(items);
+
+        console.log('### Issue List');
+
+        _.each(items.issues, function (issue) {
             console.log('*', issueToListItem(issue));
+        });
+
+        console.log('### Bug List');
+
+
+        _.each(items.bugs, function (bug) {
+            console.log('*', issueToListItem(bug));
         });
     });
 }
